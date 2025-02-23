746bbea9b31010fa256a23f65b3cdf6ab15a75c9
Johan Smarius <jsmarius@hotmail.com>
2025-02-23 09:01:34 +0100

    Initial version

---
+++ Model/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Model;
+
+public class Book
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public DateOnly PublicationDate { get; set; }
+    public required string ISBN { get; set; }
+}
---
+++ Model/Publisher.cs
@@ -0,0 +1,7 @@
+﻿namespace Model;
+
+public class Publisher
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+}
